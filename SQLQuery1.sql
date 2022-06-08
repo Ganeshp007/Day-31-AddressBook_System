268947bef3197ae51c96fa47b3e522fcceec7dee
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-08 23:07:01 +0530

    Added: Inserted Records in 'AddressBook_Details'

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,6 +11,13 @@ create table AddressBook_Details                 -- Created Table TO Store Addre
   Address varchar(50),
   City varchar(15),
   State varchar(15),
-  PinCode int,
-  Mobile_No int
+  PinCode BIGINT,
+  Mobile_No BIGINT
 )
+
+--UC3 => Inserting Record in AddressBook_details Table 
+insert into AddressBook_Details(FirstName,LastName,Address,City,State,PinCode,Mobile_No) values
+('Ganesh','Potdar','Vikram Nagar','Latur','MH',413512,9511949586),
+('Ajay','Patil','shyam Nagar','Pune','MH',411044,9845256351),
+('Komal','Arude','Dhanukar Colony','Kothrud','MH',411038,9616452585),
+('Aniket','Gore','Mukund Nagar','Latur','MH',413512,9423658475)
