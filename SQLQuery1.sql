4d38a9442bbb1448c82e5aa02459ec66c302bb55
Ganeshp007 <potdart40.gp@gmail.com>
2022-06-08 23:22:02 +0530

    Added: Edited Existing Records in 'AddressBook_Details' Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -21,3 +21,11 @@ insert into AddressBook_Details(FirstName,LastName,Address,City,State,PinCode,Mo
 ('Ajay','Patil','shyam Nagar','Pune','MH',411044,9845256351),
 ('Komal','Arude','Dhanukar Colony','Kothrud','MH',411038,9616452585),
 ('Aniket','Gore','Mukund Nagar','Latur','MH',413512,9423658475)
+
+select * from AddressBook_Details                   -- TO see the Records In AddressBook_details Table
+
+--UC4 => Editing Existing Record in Table using their Name
+update AddressBook_Details set Address='Kalyan Nagar',City='Mumbai',PinCode=400029
+where FirstName='Ajay'
+
+select * from AddressBook_Details                  
